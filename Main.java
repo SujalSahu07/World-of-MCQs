fd78a521cd0e084e012f9a0b9f43eaf29663570e
Sujal Sahu <130498614+SujalSahu07@users.noreply.github.com>
2024-07-17 22:09:41 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,29 @@
-"public class Main { public static void main(String[] args) { System.out.println(\"Hello, World!\"); } }" 
+//package com.WorldOfMCQs;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("World of MCQs");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("Welcome to World of MCQs!");
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        frame.add(label);
+
+        JButton startButton = new JButton("Start Quiz");
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Quiz.startQuiz();
+            }
+        });
+        frame.add(startButton);
+
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+        frame.setVisible(true);
+    }
+}
